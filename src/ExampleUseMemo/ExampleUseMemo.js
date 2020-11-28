be0b66badc33f56428e0bb10adcdd0bd7fe8d567
谢天宸 <xietianchen@shimo.im>
2020-11-29 01:00:42 +0800

    chore

--- src/ExampleUseMemo/ExampleUseMemo.js
+++ src/ExampleUseMemo/ExampleUseMemo.js
@@ -8,18 +8,16 @@ import React, { useMemo, useState } from 'react';
 export default function ExampleUseMemo() {
   const [xiaohong, setXiaohong] = useState('小红在待客状态');
   const [zhilin, setZhilin] = useState('志玲在待客状态');
-
   return (
     <>
       <button onClick={()=>{setXiaohong(new Date().getTime())}}>小红</button>
-      <button onClick={() => { setZhilin(new Date().getTime() + '志玲来了') }}>志玲</button>      
-      <ChildComponent name={xiaohong}>{ zhilin}</ChildComponent>
+      <button onClick={()=>{setZhilin(new Date().getTime() + '志玲来了')}}>志玲</button>
+      <ChildComponent name={xiaohong}>{zhilin}</ChildComponent>
     </>
   );
 }
 
-function ChildComponent({ name, children }) {    
-
+function ChildComponent({ name, children }) {
   function changeXiaohong() {
     console.log('小红来了, 小红来了, 小红来了');
     return name + '小红走来了';
@@ -27,7 +25,6 @@ function ChildComponent({ name, children }) {
   // 父组件任何状态发生变化, 子组件任何方法changeXiaohong都会重新渲染一遍. 用useMemo解决.   
   // const actionXiaohong = changeXiaohong(name);
   const actionXiaohong = useMemo(() => changeXiaohong(name), [name]);
-
   return (
     <>
       <div>{actionXiaohong}</div>  
