f892cada16dda93073a49dc92c913ccb01f8d619
谢天宸 <xietianchen@shimo.im>
2020-12-01 22:35:01 +0800

    chore: useRef

---
+++ src/ExampleUseRef/ExampleUseRef.js
@@ -0,0 +1,34 @@
+//
+// useRef主要用来操作dom元素. 用法类似useState
+// 获取/控制 dom元素
+// 
+
+import React, { useRef, useState ,useEffect} from 'react';
+
+export default function ExampleUseRef() {
+  const inputElement = useRef(null)
+  const onButtonClick = () => {
+    inputElement.current.value = 'hellow, js11111';
+    console.log(inputElement);
+  }
+
+  const [text, setText] = useState('js default2222');
+  const textRef = useRef();
+
+  useEffect(() => { 
+    textRef.current = text;
+    console.log('textRef.current : ', textRef.current);
+  })
+
+  return (
+    <>
+      {/* 用法一 */}
+      <input ref={inputElement} type='text' />
+      <button onClick={onButtonClick}>在input上显示文字</button>
+      <br />
+      <br />
+      {/* 用法二 */}
+      <input value={text} onChange={(e)=>{setText(e.target.value)}} />
+    </>
+  )
+}
--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ExampleUseRef from './ExampleUseRef/ExampleUseRef';
 //import Example from './Example'
 // import ExampleRedux from './ExampleUseRedux/ExampleRedux';
-import ExampleUseMemo from './ExampleUseMemo/ExampleUseMemo';
+// import ExampleUseMemo from './ExampleUseMemo/ExampleUseMemo';
+
 
 ReactDOM.render(  
-    <ExampleUseMemo />,
+    <ExampleUseRef />,
   document.getElementById('root')
 );
 
